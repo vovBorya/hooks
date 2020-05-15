b7ec3e09e6b57c03de58745de7c7b73f716b7ef4
vovBorya <vovbor02@gmail.com>
2020-05-15 23:18:01 +0300

    useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <span>Hi</span>
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  )
+}
 
+const HookSwitcher = () => {
+
+  const [color, setColor] = useState('black');
+  const [fontSize, setFontSize] = useState(20);
+
+  return(
+    <div style={{ padding: '10px', backgroundColor: color, fontSize: fontSize, color: 'green' }}>
+      Hello
+      <button
+        onClick={() => {
+          setColor('black')
+          setFontSize(20)
+        }}>
+        Dark
+      </button>
+      <button onClick={() => {
+        setColor('white')
+        setFontSize(12)
+      }}>
+        Light
+      </button>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
